cb54d4e40416dc2367b7c29dbd1831898996a867
ralijaonafehizoro961-wq <ralijaonafehizoro961@gmail.com>
2026-05-01 16:35:49 +0300

    Added graphs showing the number of survivors

--- src/Data_exploration/exp_data_03.py
+++ src/Data_exploration/exp_data_03.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 df = pd.read_csv('data/Titanic-Dataset.csv')
 
 # number of passengers who survived
@@ -10,3 +13,13 @@ print(f"number of people who did not survive : {dead}")
 # survival percentage
 survived_pr = (survived / len(df['Survived'])) * 100
 print(f"Survival percentage : {survived_pr:.2f} %")
+
+# Create a bar chart
+counts = df['Survived'].value_counts()
+ax = counts.plot(kind='bar',color=['red', 'blue'])
+plt.title("Survivors vs Non-survivors")
+plt.xlabel("Survived (0 = Dead, 1 = Survived)")
+plt.ylabel("Number of passengers")
+for i, value in enumerate(counts):
+    ax.text(i, value, str(value), ha='center', va='bottom')
+plt.show()
