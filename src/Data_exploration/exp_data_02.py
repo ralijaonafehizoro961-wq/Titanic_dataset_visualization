1a5e5ee20d1201d65fa1e21279abccc266f95a65
ralijaonafehizoro961-wq <ralijaonafehizoro961@gmail.com>
2026-05-01 15:15:07 +0300

    calculate the average age of passengers

--- src/Data_exploration/exp_data_02.py
+++ src/Data_exploration/exp_data_02.py
@@ -7,8 +7,10 @@ print(df.describe())
 # the minimum and maximum values for Age
 min_age = df['Age'].min()
 max_age = df['Age'].max()
+mean_age = df['Age'].mean()
 
 print("******* Min & Max for Age *******")
-print(f"Valeur minimum : {min_age}")
-print(f"Valeur maximum : {max_age}")   
+print(f"minimum value : {min_age}")
+print(f"maximum value : {max_age}")
+print(f"average age value : {mean_age}")   
 print("*********************************")
