a06b21437b8d5a5a1b4b2e4690ad9971b8ce6098
ralijaonafehizoro961-wq <ralijaonafehizoro961@gmail.com>
2026-05-01 15:29:33 +0300

    calculate the number of passengers who survived or not

---
+++ src/Data_exploration/exp_data_03.py
@@ -0,0 +1,8 @@
+import pandas as pd
+df = pd.read_csv('data/tested.csv')
+
+# number of passengers who survived
+survived = df['Survived'].value_counts().get(1, 0)
+dead = df['Survived'].value_counts().get(0, 0)
+print(f"number of passengers who survived : {survived}")
+print(f"number of people who did not survive : {dead}")
