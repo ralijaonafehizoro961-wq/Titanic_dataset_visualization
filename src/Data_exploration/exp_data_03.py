ea499f6b8f381ad5d0fb77aeeaba319f0f52cdd2
ralijaonafehizoro961-wq <ralijaonafehizoro961@gmail.com>
2026-05-01 15:48:31 +0300

    calculation of the survival percentage

--- src/Data_exploration/exp_data_03.py
+++ src/Data_exploration/exp_data_03.py
@@ -6,3 +6,7 @@ survived = df['Survived'].value_counts().get(1, 0)
 dead = df['Survived'].value_counts().get(0, 0)
 print(f"number of passengers who survived : {survived}")
 print(f"number of people who did not survive : {dead}")
+
+# survival percentage
+survived_pr = (survived / len(df['Survived'])) * 100
+print(f"Survival percentage : {survived_pr:.2f} %")
