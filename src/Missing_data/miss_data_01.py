0cb41476155cb0b28a3c307fea3f6e30e6073e64
ralijaonafehizoro961-wq <ralijaonafehizoro961@gmail.com>
2026-05-02 11:50:41 +0300

    feat : Added the missing data analysis block

---
+++ src/Missing_data/miss_data_01.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df = pd.read_csv('data/Titanic-Dataset.csv')
+
+# number of missing values for each column
+print(df.isnull().sum())
+
+# Displays the number and percentage (%) of missing data.
+missing = df.isnull().sum()
+percentage = (missing / len(df)) * 100
+
+result = pd.DataFrame({
+    'Missing Values': missing,
+    'Percentage (%)': percentage
+})
+
+print(result)
+
+# Sort the columns in descending order of missing values.
