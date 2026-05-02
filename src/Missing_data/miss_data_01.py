9b58b0dcf3a6e34b0b412d9a8103f7ef689a9bec
ralijaonafehizoro961-wq <ralijaonafehizoro961@gmail.com>
2026-05-02 12:17:09 +0300

    config : code modification for the visual

--- src/Missing_data/miss_data_01.py
+++ src/Missing_data/miss_data_01.py
@@ -3,7 +3,10 @@ import pandas as pd
 df = pd.read_csv('data/Titanic-Dataset.csv')
 
 # number of missing values for each column
+print("*"*10 + " number of missing values " + "*"*10)
 print(df.isnull().sum())
+print("*"*45)
+
 
 # Displays the number and percentage (%) of missing data.
 missing = df.isnull().sum()
@@ -14,11 +17,18 @@ result = pd.DataFrame({
     'Percentage (%)': percentage
 })
 
+print("*"*10 + " the number and percentage (%) of missing data " + "*"*10)
 print(result)
+print("*"*65)
+
 
 # Sort the columns in descending order of missing values.
 sorted_missing = missing.sort_values(ascending=False)
+print("*"*10 + " the columns in descending order of missing values " + "*"*10)
 print(sorted_missing)
+print("*"*70)
 
 # the columns with the most missing data
+print("*"*10 + " the columns with the most missing data " + "*"*10)
 print(sorted_missing.head(3))
+print("*"*60)
