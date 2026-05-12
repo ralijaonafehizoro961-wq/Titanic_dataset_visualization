932283ad7eb877a6530d5fd0fd47ecf456f40e41
ralijaonafehizoro961-wq <ralijaonafehizoro961@gmail.com>
2026-05-12 09:19:39 +0300

    feat: add a graph comparison

--- src/analysis/analysis_02.py
+++ src/analysis/analysis_02.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('results/donnees_nettoyees.csv')
 
@@ -14,3 +15,15 @@ rate_class3 = class3['Survived'].mean() * 100
 print(f"survival rate for class 1 : {rate_class1:.0f} %")
 print(f"survival rate for class 2 : {rate_class2:.0f} %")
 print(f"survival rate for class 3 : {rate_class3:.0f} %")
+
+# graph comparison of survival rate for each class
+classes = [f'class1 : {rate_class1:.0f} %',f'class2 : {rate_class2:.0f} %',f'class3 : {rate_class3:.0f} %']
+rates = [rate_class1,rate_class2,rate_class3]
+
+plt.figure(figsize=(6, 4))
+plt.bar(classes, rates, color=['g', 'k','c'])
+plt.title('Titanic Survival Rate by passenger Class')
+plt.xlabel('Passenger Class')
+plt.ylabel('Survival Rate (%)')
+plt.ylim(0, 100)
+plt.show()
