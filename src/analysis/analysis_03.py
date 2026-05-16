81b874cc3fcfe7e1de52da0466f2a36b2a3fa8de
ralijaonafehizoro961-wq <ralijaonafehizoro961@gmail.com>
2026-05-16 14:17:12 +0300

    feat:add a code for generate a graphic comparaison

--- src/analysis/analysis_03.py
+++ src/analysis/analysis_03.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('results/donnees_nettoyees.csv')
 
@@ -19,3 +21,22 @@ print(f" The average age of the survivors : {avr_age:.0f} years")
 # the average age of the non-survivors
 avr_age = df[df['Survived'] == 0]['Age'].mean()   
 print(f" The average age of the non-survivors : {avr_age:.0f} years")
+
+# boxplot to compare the two groups
+plt.figure(figsize=(10, 6))
+sns.boxplot(x='Survived', y='Age', data=df, palette='Set2')
+plt.title('Age distribution according to survival', fontsize=14)
+plt.xlabel('Situation', fontsize=12)
+plt.ylabel('Âge', fontsize=12)
+plt.xticks([0, 1], ['non-survivors', 'survivors'])
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+# hisplot for Age distribution by survival status
+plt.figure(figsize=(10, 6))
+sns.histplot(data=df, x='Age', hue='Survived', multiple='stack', palette='Set2')
+plt.title('Age distribution by survival status', fontsize=14)
+plt.xlabel('Âge', fontsize=12)
+plt.ylabel('Number of passengers', fontsize=12)
+plt.legend(['non-survivors', 'Survivors'])
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
